c3652b12c971260f104e87b3d252aa2612bfdb83
Sayan-Innoraft <sayan.manna@innoraft.com>
2024-02-29 16:10:46 +0530

    FT2-77: Fixed Session Issues.

--- basics/task7/validation.php
+++ basics/task7/validation.php
@@ -1,6 +1,5 @@
 <?php
 
-session_start();
 
 // If username or password not saved in session when accessing the questions then redirects to login page.
 if(!isset($_SESSION['username']) || !isset($_SESSION['password'])){
